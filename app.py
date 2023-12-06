7151762b82740cfe4bfa7e77f3b217dd85cebca2
JoviLambton <118281871+JoviLambton@users.noreply.github.com>
2023-12-05 21:32:09 -0500

    Updated

--- app.py
+++ app.py
@@ -4,21 +4,43 @@ import pandas as pd
 import altair as alt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from nltk.tokenize import word_tokenize, sent_tokenize
 import joblib
+import spacy
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import re
+from nltk.tokenize import word_tokenize, sent_tokenize
+import nltk
 
 import nltk
 nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet') 
+nltk.download('omw-1.4')
 
+# Load spacy model
+# nlp = spacy.load("en_core_web_md")
 
+stop_words = set([word for word in stopwords.words('english') if word not in ['not','no']])
+lemmatizer = WordNetLemmatizer()
+pos_list = ['PRP',  # PRP personal pronoun I, he, she 
+            'PRP$', # PRP$ possessive pronoun my, his, hers 
+            'WP',   # WP wh-pronoun who, what 
+            'WP$',  # WP$ possessive wh-pronoun, eg- whose
+            'IN'    # IN preposition/subordinating conjunction 
+            ]
+    
 model = joblib.load('clf_nb.pkl')
 vectorizer = joblib.load('tfidf.pkl')
+
 
 # Class names
 class_names = ['Negative', 'Positive']
 
 # Function to get the sentiment analysis for a specific text
 def SentimentAnalysis(text):
+    
     # Vectorization using TF-IDF
     x_actual_matrix = vectorizer.transform([text])
     x_actual_tfidf_vector = x_actual_matrix.toarray()
@@ -55,6 +77,12 @@ def AspectBasedSentimentAnalysis(inText, aspect_list):
                 # Remove the aspect from the sentence
                 aspect_text =  sentence.replace(aspect, "")
                 
+                # Pre-process text
+                aspect_text = preprocess_text_1(aspect_text)
+                # aspect_text = remove_named_entities(aspect_text)
+                aspect_text = preprocess_text_2(aspect_text)
+                aspect_text = remove_pos(aspect_text)
+                
                 # Perform sentiment analysis on the sentence without the keyword
                 aspect_sentiment = SentimentAnalysis(aspect_text)
                 
@@ -76,15 +104,93 @@ def AspectBasedSentimentAnalysis(inText, aspect_list):
     # Return sentiment analysis for the aspect
     return aspect_class                    
 
+
+# Function to preprocess text
+def preprocess_text_1(data):                                      
+    text=str(data)                                              # Convert all text to strings
+    text=re.sub(r'(http|https)?://\S+|www\.\S+','',text)        # Remove any links
+    text = re.sub(r'[\d]+','',text)                             # Remove digits
+    text=re.sub(r'\s{2,}',' ',text)                             # Remove multiple spaces
+
+    # Deconstruct words
+    text = re.sub(r"won\'t", "will not", text)
+    text = re.sub(r"can\'t", "can not", text)
+    text = re.sub(r"n\'t", " not", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'s", " is", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'t", " not", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'m", " am", text)
+    
+    return text
+
+# Function to remove named entities
+def remove_named_entities(text):      
+        
+    document = nlp(text)
+    no_named_entities = []
+
+    for item in document:
+        # If named entity, pass
+        if item.text in [e.text for e in document.ents]:
+            pass
+        # Append non named entities
+        else:
+            no_named_entities.append(item.text)
+            
+    # Return no_named_entities
+    return " ".join(no_named_entities)
+
+# Function to preprocess text
+def preprocess_text_2(data):
+    text=str(data)                                              # Convert all text to strings
+    text=re.sub(r'[^a-zA-Z0-9\s]','',text)                      # Remove special characters
+    text=text.lower()                                           # Convert to lowercase characters
+    text=re.sub(r'[^\w\s]','',text)                             # Remove punctuation
+    text=re.sub(r'\b\w\b','',text)                              # Remove single characters
+    return text
+
+# Function to remove pronouns, prepositions, and stop words
+def remove_pos(text):
+    
+    word_list = word_tokenize(text)
+    no_pos_stop = []
+    
+    for word in word_list:
+        
+        pos_tag = nltk.pos_tag([word])
+        
+        # If pronoun or preposition or stop word, drop
+        if word in stop_words or pos_tag[0][1] in pos_list:
+            pass
+        
+        # Else, append lemmatized word
+        else:
+            no_pos_stop.append(lemmatizer.lemmatize(word))
+    
+    return " ".join(no_pos_stop)
+
+
+
 def process_file(df):
+    
     text_df = df["Review"]
+    
+    # Pre-process text
+    text_df = text_df.apply(preprocess_text_1)
+    # text_df = text_df.apply(remove_named_entities)
+    text_df = text_df.apply(preprocess_text_2)
+    text_df = text_df.apply(remove_pos)
+
     text_vectorized = vectorizer.transform(text_df)
     df["Sentiment"] = model.predict(text_vectorized)
     df["Sentiment"] = df["Sentiment"].apply(lambda x: "Negative" if x == 0 else "Positive")
     product_aspects = {}
     for i, row in df.iterrows():
         text = row["Review"]
-        aspect_sentiments = AspectBasedSentimentAnalysis(text, row["aspect_list"].split(","))
+        aspect_sentiments = AspectBasedSentimentAnalysis(text, row["Aspect_List"].split(","))
         if row["Product"] not in product_aspects:
             product_aspects[row["Product"]] = [aspect_sentiments]
         elif aspect_sentiments:
@@ -113,7 +219,7 @@ def main():
 
         if st.button("Process Text"):
             df, product_aspects  = process_file(df)
-            df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+            df['Timestamp'] = pd.to_datetime(df['Timestamp']).dt.date
 
             # Streamlit App
             st.title("Product Sentiment Analysis Report")
@@ -153,13 +259,13 @@ def main():
             # st.table(topic_counts)
 
             # Comparison Across Products/Features
-            st.header("Comparison Across Products/Features")
+            st.header("Comparison Across Products")
             if 'Product' in df.columns:
                 # Filter data based on selected products
                 product_sentiment_counts = df[['Product', 'Sentiment', 'Source']].groupby(['Product', 'Sentiment']).count().fillna(0)
                 product_sentiment_counts = product_sentiment_counts.rename(columns={"Source": "Count"})
                 product_sentiment_counts = product_sentiment_counts.reset_index()
-                st.dataframe(product_sentiment_counts)
+                # st.dataframe(product_sentiment_counts)
                 bar_chart = alt.Chart(product_sentiment_counts).mark_bar().encode(
                         x="Product",
                         y="Count",
@@ -169,7 +275,7 @@ def main():
 
                 st.table(product_sentiment_counts)
 
-            st.header("Aspects Across Products")
+            st.header("Comparison Across Features")
            
             for product, aspects in product_aspects.items():
                 st.subheader(product+":")
@@ -189,7 +295,7 @@ def main():
                     )
                 st.altair_chart(bar_chart, use_container_width=True)
 
-                st.table(product_sentiment_counts)
+                # st.table(product_sentiment_counts)
 
 
 if __name__ == "__main__":
